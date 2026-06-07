03a206b496fdc333f1fdb1d358681d801e3635d0
vivekisadev <vivekofficial79030@gmail.com>
2026-06-07 15:22:46 +0530

    changed the preview card

--- app/api/og.png/route.tsx
+++ app/api/og.png/route.tsx
@@ -86,39 +86,46 @@ export async function GET(req: NextRequest) {
               
               <div
                 style={{
-                  fontSize: 24,
-                  color: "#0969da", // GitHub blue link
-                  marginTop: 24,
-                  fontWeight: 600,
                   display: "flex",
                   alignItems: "center",
-                  gap: "8px",
+                  gap: "32px",
+                  marginTop: 32,
                 }}
               >
-                Read full at the link below
-                <svg viewBox="0 0 16 16" width="24" height="24" fill="currentColor">
-                  <path d="M8.22 2.97a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06l2.97-2.97H3.75a.75.75 0 0 1 0-1.5h7.44L8.22 4.03a.75.75 0 0 1 0-1.06Z" />
-                </svg>
-              </div>
-              
-              {/* Metadata (Comments / Reading Time) */}
-              {readingTime && (
                 <div
                   style={{
+                    fontSize: 24,
+                    color: "#0969da", // GitHub blue link
+                    fontWeight: 600,
                     display: "flex",
                     alignItems: "center",
-                    gap: "12px",
-                    marginTop: "32px",
-                    color: "#656d76",
-                    fontSize: 28,
+                    gap: "8px",
                   }}
                 >
-                  <svg viewBox="0 0 16 16" width="32" height="32" fill="currentColor">
-                    <path d="M8 1.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13ZM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Zm8-4a.75.75 0 0 1 .75.75v3.5l2.06 1.03a.75.75 0 1 1-.67 1.34l-2.5-1.25a.75.75 0 0 1-.33-.67v-4A.75.75 0 0 1 8 4Z" />
+                  Read full at the link below
+                  <svg viewBox="0 0 16 16" width="24" height="24" fill="currentColor">
+                    <path d="M8.22 2.97a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06l2.97-2.97H3.75a.75.75 0 0 1 0-1.5h7.44L8.22 4.03a.75.75 0 0 1 0-1.06Z" />
                   </svg>
-                  <span>{readingTime} min read</span>
                 </div>
-              )}
+                
+                {/* Metadata (Reading Time) */}
+                {readingTime && (
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      gap: "8px",
+                      color: "#656d76",
+                      fontSize: 24,
+                    }}
+                  >
+                    <svg viewBox="0 0 16 16" width="24" height="24" fill="currentColor">
+                      <path d="M8 1.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13ZM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Zm8-4a.75.75 0 0 1 .75.75v3.5l2.06 1.03a.75.75 0 1 1-.67 1.34l-2.5-1.25a.75.75 0 0 1-.33-.67v-4A.75.75 0 0 1 8 4Z" />
+                    </svg>
+                    <span>{readingTime} min read</span>
+                  </div>
+                )}
+              </div>
             </div>
             
             {/* Right side: Identicon */}
@@ -195,12 +202,7 @@ export async function GET(req: NextRequest) {
               </div>
             </div>
             
-            {/* GitHub Logo */}
-            <div style={{ display: "flex" }}>
-              <svg viewBox="0 0 16 16" width="32" height="32" fill="#656d76">
-                <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.46-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
-              </svg>
-            </div>
+
           </div>
         </div>
       ),
